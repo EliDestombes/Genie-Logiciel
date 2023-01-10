cfd5e4bbcda3ea27878e53c69d956876813822dd
usrlocal <usrlocal@GA319-FE21.ad.univ-angers.fr>
2023-01-10 17:08:52 +0100

    Modification des Tests par TestDossierBancaire()

---
+++ src/tests/TestsDossierBancaire.java
@@ -0,0 +1,41 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import myPackage.DossierBancaire;
+
+public class TestsDossierBancaire {
+	
+	public TestsDossierBancaire(){}
+	
+	public class MyTest1 {
+
+		@Test  
+		public void test1_1() 
+		{
+			DossierBancaire dossier=new DossierBancaire();
+			dossier.deposer(100);
+			assertEquals(100,dossier.get_solde(),0); //voir documentation en ligne sur assertions Junit 
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+		Result result = JUnitCore.runClasses(MyTest1.class);
+		for (Failure fail : result.getFailures()) 
+		{
+			System.out.println(fail.toString());
+		}
+		if (result.wasSuccessful()) 
+		{
+			System.out.println("All tests finished successfully...");
+		}
+	}
+}

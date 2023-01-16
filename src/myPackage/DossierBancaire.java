8b42c7503176bd6a7fc0a8e3f0aa16155ca7394f
usrlocal <usrlocal@GE23-FE18.ad.univ-angers.fr>
2023-01-16 15:41:55 +0100

    documentation dossier bancaire

--- src/myPackage/DossierBancaire.java
+++ src/myPackage/DossierBancaire.java
@@ -1,17 +1,17 @@
 package myPackage;
 
 public class DossierBancaire {
-	private CompteCourant CC_solde= new CompteCourant();
-	private CompteEpargne CE_solde= new CompteEpargne();
+	private CompteCourant CC_solde= new CompteCourant(); //creation compte courant
+	private CompteEpargne CE_solde= new CompteEpargne(); //creation compte epargne
 	//Constructeur
-    public DossierBancaire() {
+    public DossierBancaire() { //initialisation des soldes des comptes
     	CC_solde = 0;
     	CE_solde = 0;
     }
 
-    public void deposer(double value) {
-    	CC_solde.setSolde(value*0.4);
-    	CE_solde.setSolde(value*0.6);
+    public void deposer(double value) { 
+    	CC_solde.setSolde(value*0.4); //40% du montant envoyé au compte courant
+    	CE_solde.setSolde(value*0.6); //60% du montant envoyé au compte epargne
     }
     public double get_solde() {return (CC_solde.getSolde() + CE_solde.getSolde());}
     public void remunerer() { CE_solde.RemunererCE();}
